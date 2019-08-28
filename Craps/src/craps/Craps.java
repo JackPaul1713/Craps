6be2761ccd3809bb0556f76a231386b28400c582
Jack.Martin <Jack.Martin@MU-C1-11.mullen.pvt.k12.co.us>
2019-08-28 11:48:10 -0600

    Commit

--- Craps/src/craps/Craps.java
+++ Craps/src/craps/Craps.java
@@ -10,7 +10,7 @@ public class Craps
 		int roll2;
 		int rollSum;
 		
-		roll1 = dice();
+		roll1 = dice(roll1, roll2);
 		roll2 = dice();
 
 	}
@@ -21,15 +21,17 @@ public class Craps
 		double randomNumber = (int)(Math.random() * 10 + 1);
 		int roll = (int) randomNumber;
 		return(roll);
-		
+		//stedthedrtyhedrthedrth
 	}
 	
-	public static int diceSum(roll1, roll2)
+	public static int diceSum()
 	{
 
 		int roll1;
 		int roll2;
 		return(roll1 + roll2);
+		
+		//Test
 		
 	}
 
