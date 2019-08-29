00740cb11209c8052a730fbe14e51612b99671ec
Jack Paul <Jack Paul@192.168.2.215>
2019-08-29 09:21:04 -0600

    Commit

--- Craps/src/craps/Craps.java
+++ Craps/src/craps/Craps.java
@@ -11,28 +11,41 @@ public class Craps
 		int roll1;
 		int roll2;
 		int rollSum;
+		int rollOne = 0;
+		int monney = 100;
 		int bet;
+		int checkBet;
 		
 		boolean wantToPlay;
+		boolean gameFinished = false;
+		boolean firstRoll = true;
+		int betSucess;
 		
 		String userName;
 		
-		System.out.println("Welcome to craps.");
-		userName = welcomePlayer();
+		System.out.println("Welcome to Craps.");
 		wantToPlay = askToPlay();
 		
 		if (wantToPlay == true)
 		{
 			
+			userName = welcomePlayer();
 			displayRules();
 			
 			while (wantToPlay == true)
 			{
 				
-				bet = getBet();
-				roll1 = rollDice();
-				roll2 = rollDice();
-				rollSum = addDice(roll1, roll2);
+				bet = getBet(monney);
+				
+				while (gameFinished == false)
+				{
+					
+					roll1 = rollDice();
+					roll2 = rollDice();
+					rollSum = addDice(roll1, roll2);
+					betSucess = checkBet(firstRoll, rollSum, rollOne);
+					
+				}
 				
 			}
 			
@@ -105,12 +118,39 @@ public class Craps
 		
 	}
 	
-	public static int getBet()
+	public static int getBet(int monney)
 	{
 		
-		bet
+		int bet;
 		
-		return(bet);
+		boolean validBet = false;
+		
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Enter bet ammount.");
+		bet = input.nextInt();
+		
+		while (validBet = false)
+		{
+			
+			if (bet <= monney && bet >= 0)
+			{
+			
+				validBet = true;
+				return(bet);
+			
+			}
+			else
+			{
+			
+				System.out.println("Invalid bet.");
+				System.out.println("Enter new bet");
+				bet = input.nextInt();
+			
+			}
+			
+		}
+		return bet;
 		
 	}
 	
@@ -127,6 +167,58 @@ public class Craps
 	{
 
 		return(roll1 + roll2);
+		
+	}
+	
+	public static int checkBet(boolean firstRoll, int rollSum, int rollOne)
+	{
+		
+		if (firstRoll == true)
+		{
+			
+			if (rollSum == 7 || rollSum == 11)
+			{
+				
+				return(2);
+			
+			}
+			else if (rollSum == 2 || rollSum == 12)
+			{
+				
+				return(0);
+				
+			}
+			else
+			{
+				
+				return(1);
+				
+			}
+			
+		}
+		else if (firstRoll == false)
+		{
+			
+			if(rollSum == rollOne)
+			{
+				
+				return(2);
+				
+			}
+			else if (rollSum == 7)
+			{
+				
+				return(0);
+				
+			}
+			else
+			{
+				
+				return(1);
+				
+			}
+			
+		}
 		
 	}
 
--- Craps/src/craps/Dice.java
+++ Craps/src/craps/Dice.java
@@ -6,7 +6,7 @@ public class Dice
 	public static int dice()
 	{
 		
-		double randomNumber = (int)(Math.random() * 10 + 1);
+		double randomNumber = (int)(Math.random() * 6 + 1);
 		int roll = (int) randomNumber;
 		return(roll);
 		
