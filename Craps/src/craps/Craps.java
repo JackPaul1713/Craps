d982805dad6bb77207ff2abb6c3b5e76853eaff7
Jack Paul <Jack Paul@DESKTOP-2DQBEEA>
2019-09-03 23:33:46 -0600

    Commit 03?

--- Craps/src/craps/Craps.java
+++ Craps/src/craps/Craps.java
@@ -8,218 +8,296 @@ public class Craps
 	public static void main(String[] args)
 	{
 
-		int roll1;
-		int roll2;
-		int rollSum;
-		int rollOne = 0;
-		int monney = 100;
+		// variables
+
+		int money = 1000;
 		int bet;
-		int checkBet;
+		boolean play = true;
+		boolean win;
+		String username;
+
+		// action
+
+		username = welcomeUser();
+		displayRules();
 		
-		boolean wantToPlay;
-		boolean gameFinished = false;
-		boolean firstRoll = true;
-		int betSucess;
-		
-		String userName;
-		
-		System.out.println("Welcome to Craps.");
-		wantToPlay = askToPlay();
-		
-		if (wantToPlay == true)
+		while (play = true)
 		{
-			
-			userName = welcomePlayer();
-			displayRules();
-			
-			while (wantToPlay == true)
-			{
-				
-				bet = getBet(monney);
-				
-				while (gameFinished == false)
-				{
-					
-					roll1 = rollDice();
-					roll2 = rollDice();
-					rollSum = addDice(roll1, roll2);
-					betSucess = checkBet(firstRoll, rollSum, rollOne);
-					
-				}
-				
-			}
-			
-		}
-		else
-		{
-			
-			System.out.println("Goodbye.");
-			
+
+			bet = placeBet(money);
+			win = playCraps();
+			money = betResults(win, money, bet);
+			displayMoney(money);
+			play = playAgain();
+
 		}
 
 	}
-	
-	public static String welcomePlayer()
+
+	public static String welcomeUser()
 	{
-		
-		String userName;
-		
-		Scanner input = new Scanner(System.in);
-		
-		System.out.println("Enter a username.");
-		userName = input.nextLine();
-		System.out.println("Welcome " + userName + ".");
-		
-		return(userName);
-		
-	}
-	
-	public static boolean askToPlay()
-	{
-		
-		boolean wantToPlay = false;
-		
-		String answer;
+
+		// variables
+
 		String y = "y";
 		String n = "n";
-		
+		String response;
+		String username;
 		Scanner input = new Scanner(System.in);
-		
+
+		// action
+
+		System.out.println("Welcome to craps.");
 		System.out.println("Would you like to play? y or n.");
-		answer = input.nextLine();
-		
-		if (answer.equals("y"))
+		response = input.nextLine();
+
+		if (response.equals(y))
 		{
-			
-			wantToPlay = true;
-			
-		}
-		else if (answer.equals("n"))
+
+			System.out.println("Enter a username.");
+			username = input.nextLine();
+			System.out.println("Welcome " + username + ".");
+
+			return (username);
+
+		} 
+		else if (response.equals(n))
 		{
-			
-			wantToPlay = false;
-			
-		}
+
+			System.out.println("Goodbye.");
+			System.exit(0);
+
+		} 
 		else
 		{
-			
-			System.out.println("Invalid input.");
-			
+
+			for (int i = 0; i <= 10; i++)
+			{
+
+				System.out.println("Alert! Alert!");
+				System.out.println("Invaild input!");
+
+			}
+
 		}
-		
-		return (wantToPlay);
-		
+
+		return ("");
+
 	}
-	
+
 	public static void displayRules()
 	{
-		
-		System.out.println("Display rules here.");
-		
-	}
-	
-	public static int getBet(int monney)
-	{
-		
-		int bet;
-		
-		boolean validBet = false;
-		
+
+		// variables
+
+		String enterToContinue;
 		Scanner input = new Scanner(System.in);
-		
-		System.out.println("Enter bet ammount.");
+
+		// action
+
+		System.out.println("\nRULES");
+		System.out.println("You start with $1000 dollars of useless curency.");
+		System.out.println("You can place a bet on whether or not you win the game.");
+		System.out.println(
+				"If you win you will recive an additional amount equal to your bet, but if you loose you loose an amount equal to your bet.");
+		System.out.println("When the game starts, you roll two dice and add them together.");
+		System.out.println(
+				"If you roll a 7 or an 11 you automaticaly win, but if you roll a 2 or 12 you automaticay loose.");
+		System.out.println(
+				"If your roll equals anything else, you roll continuously unil you either get the same number, in which case you win, or until you get a 7, in which case you loose.");
+		System.out.println("Press enter to continue.");
+		enterToContinue = input.nextLine();
+
+	}
+
+	public static int placeBet(int maxBet)
+	{
+
+		// variables
+
+		int bet;
+		boolean vallidBet = false;
+		String wantToBet;
+		Scanner input = new Scanner(System.in);
+
+		// action
+
+		System.out.println("Enter a bet.");
 		bet = input.nextInt();
-		
-		while (validBet = false)
+
+		while (vallidBet == false)
 		{
 			
-			if (bet <= monney && bet >= 0)
+			if (bet >= 0 && bet <= maxBet)
 			{
-			
-				validBet = true;
-				return(bet);
-			
-			}
+
+				vallidBet = true;
+				return (bet);
+
+			} 
 			else
 			{
-			
+
 				System.out.println("Invalid bet.");
-				System.out.println("Enter new bet");
+				System.out.println("Enter a new bet.");
 				bet = input.nextInt();
-			
+
 			}
+
+		}
+
+		return (0);
+
+	}
+
+	public static boolean playCraps()
+	{
+
+		// variables
+
+		int firstRoll;
+		int otherRoll;
+		int rollPt1;
+		int rollPt2;
+		boolean end = false;
+		String enterToRoll;
+		Scanner input = new Scanner(System.in);
+
+		// action
+
+		System.out.println("Press enter to roll for the first time.");
+		enterToRoll = input.nextLine();
+		rollPt1 = rollDice();
+		rollPt2 = rollDice();
+		firstRoll = rollPt1 + rollPt2;
+		System.out.println("You rolled a " + firstRoll + ".");
+
+		if (firstRoll == 7 || firstRoll == 11)
+		{
+
+			return (true);
+
+		} 
+		else if (firstRoll == 2 || firstRoll == 12)
+		{
+
+			return (false);
+
+		} 
+		else
+		{
+
+			while (end == false)
+			{
+
+				System.out.println("Press enter to roll again.");
+				enterToRoll = input.nextLine();
+				otherRoll = 0;
+				rollPt1 = rollDice();
+				rollPt2 = rollDice();
+				otherRoll = rollPt1 + rollPt2;
+				System.out.println("You rolled a " + otherRoll + ".");
+
+				if (otherRoll == firstRoll)
+				{
+
+					end = true;
+					return (true);
+
+				} else if (otherRoll == 7)
+				{
+
+					end = true;
+					return (false);
+
+				}
+
+			}
+
+		}
+		
+		return(false);
+
+	}
+
+	public static int betResults(boolean win, int money, int bet)
+	{
+
+		if (win == true)
+		{
+
+			System.out.println("Congrats you won! You win " + bet + " dollars.");
+			money = money + bet;
+
+		} 
+		else if (win == false)
+		{
+
+			System.out.println("You lost! Haha. You lose " + bet + " dollars.");
+			money = money - bet;
+
+		}
+		
+		if (money == 0)
+		{
+			
+			System.out.println("Sorry but you lost all your money, so you can't play anymore");
+			System.out.println("Goodbye.");
+			System.exit(0);
 			
 		}
-		return bet;
+
+		return (money);
+
+	}
+	
+	public static void displayMoney(int money)
+	{
+		
+		System.out.println("Now you have " + money + " dollars.");
+		
+	}
+
+	public static boolean playAgain()
+	{
+		
+		//variables
+		
+		String y = "y";
+		String n = "n";
+		String response;
+		Scanner input = new Scanner(System.in);
+		
+		//action
+		
+		System.out.println("Would you like to play again? y or n.");
+		response = input.nextLine();
+		
+		if (response.equals(y))
+		{
+			
+			return (true);
+			
+		}
+		else if (response.equals(n))
+		{
+			
+			System.out.println("Thanks for playing, Goodbye.");
+			System.exit(0);
+			
+		}
+		
+		return (false);
 		
 	}
 	
 	public static int rollDice()
 	{
-		
-		double randomNumber = (int)(Math.random() * 10 + 1);
-		int roll = (int) randomNumber;
-		return(roll);
-		
-	}
-	
-	public static int addDice(int roll1, int roll2)
-	{
 
-		return(roll1 + roll2);
-		
-	}
-	
-	public static int checkBet(boolean firstRoll, int rollSum, int rollOne)
-	{
-		
-		if (firstRoll == true)
-		{
-			
-			if (rollSum == 7 || rollSum == 11)
-			{
-				
-				return(2);
-			
-			}
-			else if (rollSum == 2 || rollSum == 12)
-			{
-				
-				return(0);
-				
-			}
-			else
-			{
-				
-				return(1);
-				
-			}
-			
-		}
-		else if (firstRoll == false)
-		{
-			
-			if(rollSum == rollOne)
-			{
-				
-				return(2);
-				
-			}
-			else if (rollSum == 7)
-			{
-				
-				return(0);
-				
-			}
-			else
-			{
-				
-				return(1);
-				
-			}
-			
-		}
-		
+		double randomNumber = (int) (Math.random() * 6 + 1);
+		int roll = (int) randomNumber;
+		return (roll);
+
 	}
 
 }
